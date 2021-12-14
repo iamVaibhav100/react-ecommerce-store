e22116ba955aa6eb85812129d2aaaca02f8b5acf
Vaibhav Shukla <vaibhavshukla1967@gmail.com>
2021-12-14 13:05:57 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Announcement from './components/Announcement';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <div>
     <Announcement/>
     <Navbar />
+    <Slider/>
     </div>
   );
 }
