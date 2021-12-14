6bb97ab8b64a6467d1de90da28f57f0f0e5a8a38
Vaibhav Shukla <vaibhavshukla1967@gmail.com>
2021-12-14 18:58:38 +0530

    added categories component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import Announcement from './components/Announcement';
-import Navbar from './components/Navbar';
-import Slider from './components/Slider';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-    <Announcement/>
-    <Navbar />
-    <Slider/>
-    </div>
+    <Home/>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Announcement from "../components/Announcement"
+import Categories from "../components/Categories";
+import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
-    return (
-        <div>
-            HomePage
-        </div>
-    )
-}
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories/>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
