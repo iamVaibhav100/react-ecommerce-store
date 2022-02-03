d13d722414c1a7a426c6f1162549b3fc45a60cef
Vaibhav Shukla <vaibhavshukla1967@gmail.com>
2022-02-03 14:09:49 +0530

    completed cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@material-ui/icons";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
@@ -47,8 +48,100 @@ const Info = styled.div`
   flex: 3;
 `;
 
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+  display: flex;
+  flex: 2;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductID = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 28px;
+  font-weight: 300;
+`;
+
+const Hr = styled.hr`
+  background-color: #e0e0e0;
+  border: none;
+  height: 1px;
+`;
+
 const Summary = styled.div`
   flex: 1;
+  border: 0.5px solid #e0e0e0;
+  padding: 20px;
+  border-radius: 10px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const SummaryButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #000;
+  color: #fff;
+  font-weight: 600;
 `;
 
 const Cart = () => {
@@ -67,8 +160,79 @@ const Cart = () => {
           <TopButton type="filled">CHECKOUT NOW</TopButton>
         </Top>
         <Bottom>
-          <Info>Info</Info>
-          <Summary>Summary</Summary>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://simg.nicepng.com/png/small/8-81778_nike-running-shoes-png-picture-shoes-nike-png.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>NIKE Premium Shoes
+                  </ProductName>
+                  <ProductID>
+                    <b>ID: </b>385967359863
+                  </ProductID>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size: </b>40
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 45</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://www.pngmart.com/files/13/Black-Converse-Shoes-Transparent-Background.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>NIKE Premium Shoes
+                  </ProductName>
+                  <ProductID>
+                    <b>ID: </b>385967359863
+                  </ProductID>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size: </b>40
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 45</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 10.5</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -10.5</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryButton>CHECKOUT NOW</SummaryButton>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
